085a1041e59a0146d7bbbeb31d30fac92267503b
nooraphussain <noorahussainp2002@gmail.com>
2026-03-06 16:52:36 +0530

    updating main

--- main.js
+++ main.js
@@ -1,13 +1,27 @@
-// main.js
+//main.js
 const { app, BrowserWindow } = require("electron");
 
 function createWindow() {
   const win = new BrowserWindow({
-    width: 800,
-    height: 600
+    title: "My little calendar",
+    width: 214,
+    height: 228,
+    resizable: false,
+    maximizable: false,
+    fullscreenable: false,
+    frame: false, 
+    transparent: true,
+    background: "#00000000",
+    webPreferences: {
+      contextIsolation: true
+    }
   });
 
   win.loadFile("index.html");
 }
 
 app.whenReady().then(createWindow);
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") app.quit();
+});
